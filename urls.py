67a6bd1bb04f39767dbd0b1273fbedf5931365f8
Adam Patterson <adam@adamrt.com>
2014-10-13 15:23:59 -0700

    pep8: indent code 4 spaces

--- urls.py
+++ urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^register/$', views.RegisterUser.as_view(), name='register-user'),
-  url(r'^login/$', views.LoginUser.as_view(), name='login-user'),
-  url(r'^logout/$', views.LogoutUser.as_view(), name='logout-user'),
-  url(r'^password/$', views.PasswordChangeUser.as_view(),
-    name='password-change-user'),
-  url(r'^reset/$', views.PasswordResetUser.as_view(),
-    name='password-reset-user'),
+    url(r'^register/$', views.RegisterUser.as_view(), name='register-user'),
+    url(r'^login/$', views.LoginUser.as_view(), name='login-user'),
+    url(r'^logout/$', views.LogoutUser.as_view(), name='logout-user'),
+    url(r'^password/$', views.PasswordChangeUser.as_view(),
+        name='password-change-user'),
+    url(r'^reset/$', views.PasswordResetUser.as_view(),
+        name='password-reset-user'),
 ]
